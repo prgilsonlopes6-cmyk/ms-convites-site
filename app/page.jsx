1ef70c8f8ea327224a4828c9f8b98c70d56d02fd
prgilsonlopes6-cmyk <prgilsonlopes6@gmail.com>
2026-04-15 16:20:33 -0300

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return (
+    <div style={{background:'black',color:'white',height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+      <h1>Convites Digitais Mainã Silvia</h1>
+    </div>
+  )
+}
